b26b0198eb880c1ca925afd46c98f39b74177914
Patrick Baselier <patrick.baselier@gmail.com>
2020-11-15 21:07:42 +0100

    Add bootstrap

--- lib/generators/bs5/install/install_generator.rb
+++ lib/generators/bs5/install/install_generator.rb
@@ -1,40 +1,23 @@
 # frozen_string_literal: true
 
 class Bs5::InstallGenerator < Rails::Generators::Base
-  source_root File.expand_path('templates', __dir__)
-
-  def install_stimulus
-    rails_command 'webpacker:install:stimulus'
-  end
-
   def add_bootstrap
     run 'yarn add bootstrap@next popper.js'
-  end
 
-  def update_and_configure_css
-    create_file 'app/javascript/packs/styles.scss', <<~EOF
+    create_file 'app/javascript/packs/styles.scss', <<~HEREDOC
       @import "~bootstrap/scss/bootstrap";
-    EOF
+    HEREDOC
 
     inject_into_file 'app/views/layouts/application.html.erb', before: '</head>' do
-      <<~ERB
+      <<~HEREDOC
         <%= stylesheet_pack_tag 'styles', media: 'all', 'data-turbolinks-track': 'reload' %>
-      ERB
+      HEREDOC
     end
+
+    append_file 'app/javascript/packs/application.js', <<~HEREDOC
+      import "bootstrap";
+    HEREDOC
 
     gsub_file 'config/webpacker.yml', 'extract_css: false', 'extract_css: true'
   end
-
-  # def stimulus_controller
-  #   create_file 'app/javascript/controllers/alert_controller.js', <<~EOF
-  #     import { Controller } from "stimulus";
-  #     import Alert from "bootstrap/js/dist/alert";
-
-  #     export default class extends Controller {
-  #       connect() {
-  #         new Alert(this.element);
-  #       }
-  #     }
-  #   EOF
-  # end
 end
