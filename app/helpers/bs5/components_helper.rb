b8a0b1ad05df860a312f7963208ed6de5314a4e8
Patrick Baselier <patrick.baselier@gmail.com>
2020-11-22 11:45:14 +0100

    Refactor helper methods

--- app/helpers/bs5/components_helper.rb
+++ app/helpers/bs5/components_helper.rb
@@ -2,29 +2,42 @@
 
 module Bs5
   module ComponentsHelper
-    def bs5_accordion(*args)
-      render AccordionComponent.new(*args) do |accordion|
-        yield accordion if block_given?
-      end
+    def bs5_accordion(*args, &block)
+      render_component(AccordionComponent, *args, &block)
     end
 
-    def bs5_alert(*args)
-      render AlertComponent.new(*args) do
-        yield if block_given?
-      end
+    def bs5_alert(*args, &block)
+      render_component(AlertComponent, *args, &block)
     end
 
-    def bs5_badge(*args)
-      render BadgeComponent.new(*args)
+    def bs5_badge(*args, &block)
+      render_component(BadgeComponent, *args, &block)
     end
 
-    def bs5_close_button(*args)
-      render CloseButtonComponent.new(*args)
+    def bs5_close_button(*args, &block)
+      render_component(CloseButtonComponent, *args, &block)
     end
 
-    def bs5_breadcrumb(*args)
-      render BreadcrumbComponent.new(*args) do |breadcrumb|
-        yield breadcrumb if block_given?
+    def bs5_breadcrumb(*args, &block)
+      render_component(BreadcrumbComponent, *args, &block)
+    end
+
+    # def bs5_button_tag(content_or_options = nil, options = nil, &block)
+    #   if content_or_options.is_a? Hash
+    #     content_or_options.merge!(class: 'btn btn-primary')
+    #   else
+    #     options ||= {}
+    #     options.merge!(class: 'btn btn-primary')
+    #   end
+
+    #   button_tag(content_or_options, options, &block)
+    # end
+
+    private
+
+    def render_component(component_clazz, *args)
+      render component_clazz.new(*args) do |c|
+        yield c if block_given?
       end
     end
   end
