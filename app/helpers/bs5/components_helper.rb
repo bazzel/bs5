ebc5fa4c35c7a25ac225b0a71439058ec0e47249
Patrick Baselier <patrick.baselier@gmail.com>
2020-11-23 08:37:57 +0100

    Metaprogram helper methods

--- app/helpers/bs5/components_helper.rb
+++ app/helpers/bs5/components_helper.rb
@@ -2,24 +2,13 @@
 
 module Bs5
   module ComponentsHelper
-    def bs5_accordion(*args, &block)
-      render_component(AccordionComponent, *args, &block)
-    end
+    COMPONENTS = %w[accordion alert badge close_button breadcrumb].freeze
 
-    def bs5_alert(*args, &block)
-      render_component(AlertComponent, *args, &block)
-    end
-
-    def bs5_badge(*args, &block)
-      render_component(BadgeComponent, *args, &block)
-    end
-
-    def bs5_close_button(*args, &block)
-      render_component(CloseButtonComponent, *args, &block)
-    end
-
-    def bs5_breadcrumb(*args, &block)
-      render_component(BreadcrumbComponent, *args, &block)
+    COMPONENTS.each do |name|
+      define_method("bs5_#{name}") do |*args, &block|
+        clazz = "Bs5::#{name.classify}Component".constantize
+        render_component(clazz, *args, &block)
+      end
     end
 
     # def bs5_button_tag(content_or_options = nil, options = nil, &block)
