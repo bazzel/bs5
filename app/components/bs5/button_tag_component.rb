5dcef5e30ec1fe928a1f1e6b52980590abcd095c
Patrick Baselier <patrick.baselier@gmail.com>
2020-11-25 16:22:59 +0100

    Add outline option

--- app/components/bs5/button_tag_component.rb
+++ app/components/bs5/button_tag_component.rb
@@ -3,17 +3,19 @@
 module Bs5
   class ButtonTagComponent < ViewComponent::Base
     STYLES = %i[primary secondary success danger warning info light dark link].freeze
+    SIZES = %i[small large].freeze
 
-    attr_reader :content_or_options, :options
+    attr_reader :content_or_options, :options, :size
 
     include ActiveModel::Validations
     validates :style, style: true
+    validates :size, inclusion: { in: SIZES, valid_sizes: SIZES.to_sentence, allow_nil: true }
 
     def initialize(content_or_options = nil, options = nil)
       @content_or_options = content_or_options.dup
       @options = options.dup
 
-      extract_style
+      extract_custom_options
       merge_default_options
     end
 
@@ -31,6 +33,12 @@ module Bs5
 
     private
 
+    def extract_custom_options
+      extract_style
+      extract_outline
+      extract_size
+    end
+
     def extract_style
       if @content_or_options.is_a? Hash
         @style = @content_or_options.delete(:style)
@@ -41,12 +49,32 @@ module Bs5
       end
     end
 
+    def extract_outline
+      if @content_or_options.is_a? Hash
+        @outline = @content_or_options.delete(:outline)
+        @content_or_options = nil if @content_or_options.empty?
+      elsif @options.is_a? Hash
+        @outline = @options.delete(:outline)
+        @options = nil if @options.empty?
+      end
+    end
+
+    def extract_size
+      if @content_or_options.is_a? Hash
+        @size = @content_or_options.delete(:size)
+        @content_or_options = nil if @content_or_options.empty?
+      elsif @options.is_a? Hash
+        @size = @options.delete(:size)
+        @options = nil if @options.empty?
+      end
+    end
+
     def merge_default_options
       if @content_or_options.is_a? Hash
-        @content_or_options.merge! default_options
+        deep_merge_and_join @content_or_options, default_options
       else
         @options ||= {}
-        @options.merge! default_options
+        deep_merge_and_join @options, default_options
       end
     end
 
@@ -57,15 +85,37 @@ module Bs5
     end
 
     def button_class
-      ['btn', contextual_class]
+      ['btn', contextual_class, size_class].compact.join(' ')
     end
 
     def contextual_class
-      "btn-#{style}"
+      ['btn', outline? ? 'outline' : nil, style].compact.join('-')
+    end
+
+    def size_class
+      return unless size?
+
+      x = { large: 'lg', small: 'sm' }[@size]
+
+      "btn-#{x}"
     end
 
     def style
       (@style || 'primary').to_sym
+    end
+
+    def outline?
+      !!@outline
+    end
+
+    def size?
+      !!@size
+    end
+
+    def deep_merge_and_join(h1, h2)
+      h1.deep_merge!(h2) do |key, this_val, other_val|
+        [this_val, other_val].join(' ').strip
+      end
     end
   end
 end
--- spec/helpers/bs5/bs5/bs5_button_tag_spec.rb
+++ spec/helpers/bs5/bs5/bs5_button_tag_spec.rb
@@ -50,6 +50,63 @@ module Bs5
           }
         end
       end
+
+      describe 'with `outline` specified' do
+        subject { helper.bs5_button_tag(options) }
+        let(:options) { { outline: true } }
+
+        it {
+          is_expected.to have_selector('button.btn.btn-outline-primary[name="button"][type="submit"]', text: 'Button')
+        }
+
+        describe 'with `style` specified' do
+          let(:options) { { outline: true, style: 'warning' } }
+
+          it {
+            is_expected.to have_selector('button.btn.btn-outline-warning[name="button"][type="submit"]', text: 'Button')
+          }
+        end
+      end
+
+      describe 'with `size` specified' do
+        describe 'large' do
+          subject { helper.bs5_button_tag(options) }
+          let(:options) { { size: :large } }
+
+          it {
+            is_expected.to have_selector('button.btn.btn-primary.btn-lg[name="button"][type="submit"]', text: 'Button')
+          }
+        end
+
+        describe 'small' do
+          subject { helper.bs5_button_tag(options) }
+          let(:options) { { size: :small } }
+
+          it {
+            is_expected.to have_selector('button.btn.btn-primary.btn-sm[name="button"][type="submit"]', text: 'Button')
+          }
+        end
+
+        describe 'invalid' do
+          subject { helper.bs5_button_tag(options) }
+          let(:options) { { size: :invalid } }
+
+          it {
+            expect do
+              subject
+            end.to raise_error(RuntimeError)
+          }
+        end
+      end
+
+      describe 'with extra options' do
+        subject { helper.bs5_button_tag('Button', options) }
+        let(:options) { { class: 'lorem', data: { toggle: 'button' } } }
+
+        it {
+          is_expected.to have_selector('button.btn.btn-primary.lorem[name="button"][data-toggle="button"]', text: 'Button')
+        }
+      end
     end
   end
 end
