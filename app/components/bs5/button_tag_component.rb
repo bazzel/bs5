ff792eea83b4731dfe96a6bddb384ec7488186d2
Patrick Baselier <patrick.baselier@gmail.com>
2020-11-27 09:43:52 +0100

    Refactor

--- app/components/bs5/button_tag_component.rb
+++ app/components/bs5/button_tag_component.rb
@@ -3,17 +3,23 @@
 module Bs5
   class ButtonTagComponent < ViewComponent::Base
     STYLES = %i[primary secondary success danger warning info light dark link].freeze
-    SIZES = %i[small large].freeze
+    DEFAULT_STYLE = :primary
+    SIZES = { small: :sm, large: :lg }.freeze
+    CLASS_PREFIX = 'btn'
 
-    attr_reader :content_or_options, :options, :size
+    attr_reader :content_or_options, :size
 
     include ActiveModel::Validations
     validates :style, style: true
-    validates :size, inclusion: { in: SIZES, valid_sizes: SIZES.to_sentence, allow_nil: true }
+    validates :size, inclusion: { in: SIZES.keys, valid_sizes: SIZES.keys.to_sentence, allow_nil: true }
 
     def initialize(content_or_options = nil, options = nil)
-      @content_or_options = content_or_options.dup
-      @options = options.dup
+      if content_or_options.is_a? Hash
+        self.options = content_or_options
+      else
+        @content_or_options = content_or_options
+        self.options = options
+      end
 
       extract_custom_options
       merge_default_options
@@ -33,6 +39,14 @@ module Bs5
 
     private
 
+    def options=(value)
+      @options = Hash(value).symbolize_keys!
+    end
+
+    def options
+      @options.empty? ? nil : @options
+    end
+
     def extract_custom_options
       extract_style
       extract_outline
@@ -40,68 +54,43 @@ module Bs5
     end
 
     def extract_style
-      if @content_or_options.is_a? Hash
-        @style = @content_or_options.delete(:style)
-        @content_or_options = nil if @content_or_options.empty?
-      elsif @options.is_a? Hash
-        @style = @options.delete(:style)
-        @options = nil if @options.empty?
-      end
+      @style = @options.delete(:style)
     end
 
     def extract_outline
-      if @content_or_options.is_a? Hash
-        @outline = @content_or_options.delete(:outline)
-        @content_or_options = nil if @content_or_options.empty?
-      elsif @options.is_a? Hash
-        @outline = @options.delete(:outline)
-        @options = nil if @options.empty?
-      end
+      @outline = @options.delete(:outline)
     end
 
     def extract_size
-      if @content_or_options.is_a? Hash
-        @size = @content_or_options.delete(:size)
-        @content_or_options = nil if @content_or_options.empty?
-      elsif @options.is_a? Hash
-        @size = @options.delete(:size)
-        @options = nil if @options.empty?
-      end
+      @size = @options.delete(:size)
     end
 
     def merge_default_options
-      if @content_or_options.is_a? Hash
-        deep_merge_and_join @content_or_options, default_options
-      else
-        @options ||= {}
-        deep_merge_and_join @options, default_options
+      @options.deep_merge!(default_options) do |_key, this_val, other_val|
+        [this_val, other_val].join(' ').strip
       end
     end
 
     def default_options
-      {
-        class: button_class
-      }
+      { class: button_class }
     end
 
     def button_class
-      ['btn', contextual_class, size_class].compact.join(' ')
+      [CLASS_PREFIX, contextual_class, size_class].compact.join(' ')
     end
 
     def contextual_class
-      ['btn', outline? ? 'outline' : nil, style].compact.join('-')
+      [CLASS_PREFIX, outline? ? 'outline' : nil, style].compact.join('-')
     end
 
     def size_class
       return unless size?
 
-      x = { large: 'lg', small: 'sm' }[@size]
-
-      "btn-#{x}"
+      [CLASS_PREFIX, SIZES[size]].join('-')
     end
 
     def style
-      (@style || 'primary').to_sym
+      (@style || DEFAULT_STYLE).to_sym
     end
 
     def outline?
@@ -109,13 +98,7 @@ module Bs5
     end
 
     def size?
-      !!@size
-    end
-
-    def deep_merge_and_join(h1, h2)
-      h1.deep_merge!(h2) do |key, this_val, other_val|
-        [this_val, other_val].join(' ').strip
-      end
+      !!size
     end
   end
 end
