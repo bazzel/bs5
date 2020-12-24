93d60d12437520a8c7c7047c9f73a4ed26889874
Patrick Baselier <patrick.baselier@gmail.com>
2020-12-24 16:11:24 +0100

    Refactor

--- app/components/bs5/dropdown_component.rb
+++ app/components/bs5/dropdown_component.rb
@@ -1,17 +1,26 @@
 # frozen_string_literal: true
 
 module Bs5
-  class DropdownComponent < ViewComponent::Base
+  class DropdownComponent < ViewComponent::Base # rubocop:disable Metrics/ClassLength
     include ViewComponent::SlotableV2
     include ComponentsHelper
     using HashRefinement
 
     CLASS_PREFIX = 'dropdown'
+    CLASS_NAMES = {
+      visually_hidden: 'visually-hidden',
+      toggle_split: "#{CLASS_PREFIX}-toggle-split",
+      toggle: "#{CLASS_PREFIX}-toggle",
+      menu: "#{CLASS_PREFIX}-menu",
+      menu_dark: "#{CLASS_PREFIX}-menu-dark",
+      menu_end: "#{CLASS_PREFIX}-menu-end"
+    }.freeze
     DIRECTIONS = {
       up: :dropup,
       end: :dropend,
       start: :dropstart
     }.with_indifferent_access.freeze
+    DROPDOWN_OPTIONS = %i[offset flip boundary reference display].freeze
 
     renders_many :items, Bs5::Dropdown::ItemComponent
     attr_reader :title
@@ -35,11 +44,33 @@ module Bs5
       @direction = @options.delete(:direction)
       @align = @options.delete(:align)
 
-      extract_data_options
+      extract_dropdown_options
     end
 
-    def extract_data_options
-      @data_options = @options.extract!(:offset, :flip, :boundary, :reference, :display)
+    def extract_dropdown_options
+      @dropdown_options = @options.extract!(*DROPDOWN_OPTIONS)
+    end
+
+    def split_button_toggle
+      bs5_button_tag(split_button_toggle_options) do
+        tag.span('Toggle Dropdown', class: CLASS_NAMES[:visually_hidden])
+      end
+    end
+
+    def split_button
+      bs5_button_tag title, split_button_options
+    end
+
+    def single_button
+      bs5_button_tag title, single_button_options
+    end
+
+    def menu_content
+      if items.any?
+        tag.ul(class: dropdown_menu_classes) { items.map(&method(:concat)) }
+      else
+        tag.div(content, class: dropdown_menu_classes)
+      end
     end
 
     def single_button_options
@@ -59,20 +90,14 @@ module Bs5
 
     def split_button_toggle_options
       single_button_options.dup.tap do |h|
-        h[:class] += ' dropdown-toggle-split'
+        h[:class] += " #{CLASS_NAMES[:toggle_split]}"
       end
     end
 
-    def split?
-      !!@split
-    end
-
-    def dark?
-      !!@dark
-    end
-
-    def direction?
-      !!@direction
+    %i[split dark direction].each do |name|
+      define_method("#{name}?") do
+        !instance_variable_get("@#{name}").nil?
+      end
     end
 
     def dropstart?
@@ -85,23 +110,21 @@ module Bs5
 
     def default_options
       default_button_options.merge({
-                                     data: default_data_options.merge(@data_options).prefix_keys_with_bs,
+                                     data: default_data_options.merge(@dropdown_options).prefix_keys_with_bs,
                                      aria: { expanded: false },
-                                     class: "#{CLASS_PREFIX}-toggle"
+                                     class: CLASS_NAMES[:toggle]
                                    })
     end
 
     def default_data_options
-      default_data_options = { toggle: :dropdown }
-
-      default_data_options[:display] = 'static' if responsive_align?
-
-      default_data_options
+      { toggle: :dropdown }.tap do |h|
+        h[:display] = 'static' if responsive_align?
+      end
     end
 
     def dropdown_menu_classes
-      class_names = ["#{CLASS_PREFIX}-menu"]
-      class_names << "#{CLASS_PREFIX}-menu-dark" if dark?
+      class_names = [CLASS_NAMES[:menu]]
+      class_names << CLASS_NAMES[:menu_dark] if dark?
       class_names << align_classes
 
       class_names.compact.join(' ')
@@ -110,7 +133,7 @@ module Bs5
     def align_classes
       case @align
       when Symbol
-        'dropdown-menu-end'
+        CLASS_NAMES[:menu_end]
       when Hash
         responsive_align_classes
       end
@@ -118,8 +141,8 @@ module Bs5
 
     def responsive_align_classes
       k, v = @align.first
-      class_names = ["dropdown-menu-#{v}-#{k}"]
-      class_names << 'dropdown-menu-end' if @align.with_indifferent_access.key?(:start)
+      class_names = ["#{CLASS_PREFIX}-menu-#{v}-#{k}"]
+      class_names << CLASS_NAMES[:menu_end] if @align.with_indifferent_access.key?(:start)
 
       class_names
     end
