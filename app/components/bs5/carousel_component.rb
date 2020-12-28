7bc0328a6feae016ec88e34f6d0434b7b66d9131
Patrick Baselier <patrick.baselier@gmail.com>
2020-12-28 11:31:45 +0100

    Add indicators

--- app/components/bs5/carousel_component.rb
+++ app/components/bs5/carousel_component.rb
@@ -6,5 +6,33 @@ module Bs5
     include ViewComponent::SlotableV2
 
     renders_many :items, Bs5::Carousel::ItemComponent
+
+    def initialize(options = {})
+      @options = options.symbolize_keys
+      extract_options
+    end
+
+    private
+
+    def extract_options
+      @controls = @options.delete(:controls)
+      @indicators = @options.delete(:indicators)
+
+      # extract_carousel_options
+    end
+
+    def id
+      "carousel-#{object_id}"
+    end
+
+    def items_count
+      items.size
+    end
+
+    %i[controls indicators].each do |name|
+      define_method("#{name}?") do
+        !instance_variable_get("@#{name}").nil?
+      end
+    end
   end
 end
---
+++ spec/helpers/bs5/bs5/bs5_carousel_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module Bs5
+  RSpec.describe ComponentsHelper, type: :helper do
+    describe '#bs5_carousel' do
+      subject do
+        helper.bs5_carousel(options) do |a|
+          a.item { tag.div('Lorem Ipsum') }
+          a.item { tag.div('Dolor Sit') }
+        end
+      end
+
+      let(:options) { {} }
+
+      it { is_expected.to have_selector('.carousel.slide[id^="carousel-"][data-bs-ride="carousel"]') }
+      it { is_expected.to have_selector('.carousel .carousel-inner .carousel-item', count: 2) }
+
+      describe 'items' do
+        it { is_expected.to have_selector('.carousel .carousel-inner .carousel-item.active div.d-block.w-100', text: 'Lorem Ipsum') }
+        it { is_expected.to have_selector('.carousel .carousel-inner .carousel-item:not(.active) div.d-block.w-100', text: 'Dolor Sit') }
+      end
+
+      describe 'with controls' do
+        let(:options) { { controls: true } }
+
+        it { is_expected.to have_selector('.carousel.slide a.carousel-control-prev[href^="#carousel-"][role="button"][data-bs-slide="prev"] span.carousel-control-prev-icon[aria-hidden="true"] + span.visually-hidden') }
+        it { is_expected.to have_selector('.carousel.slide a.carousel-control-next[href^="#carousel-"][role="button"][data-bs-slide="next"] span.carousel-control-next-icon[aria-hidden="true"] + span.visually-hidden') }
+      end
+
+      describe 'with indicators' do
+        let(:options) { { indicators: true } }
+
+        it { is_expected.to have_selector('.carousel.slide ol.carousel-indicators') }
+
+        describe 'items' do
+          it { is_expected.to have_selector('.carousel .carousel-indicators li[data-bs-target^="#carousel-"]', count: 2) }
+          it { is_expected.to have_selector('.carousel .carousel-indicators li.active[data-bs-slide-to="0"]') }
+          it { is_expected.to have_selector('.carousel .carousel-indicators li:not(.active)[data-bs-slide-to="1"]') }
+        end
+      end
+    end
+  end
+end
