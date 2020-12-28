0893f251e5d36bb1663b6f9ec356fb7cfe221b7d
Patrick Baselier <patrick.baselier@gmail.com>
2020-12-28 11:58:51 +0100

    Add crossfade

--- app/components/bs5/carousel_component.rb
+++ app/components/bs5/carousel_component.rb
@@ -17,6 +17,7 @@ module Bs5
     def extract_options
       @controls = @options.delete(:controls)
       @indicators = @options.delete(:indicators)
+      @crossfade = @options.delete(:crossfade)
 
       # extract_carousel_options
     end
@@ -29,7 +30,13 @@ module Bs5
       items.size
     end
 
-    %i[controls indicators].each do |name|
+    def component_class
+      class_names = %w[carousel slide]
+      class_names << %w[carousel-fade] if crossfade?
+      class_names.join(' ')
+    end
+
+    %i[controls indicators crossfade].each do |name|
       define_method("#{name}?") do
         !instance_variable_get("@#{name}").nil?
       end
