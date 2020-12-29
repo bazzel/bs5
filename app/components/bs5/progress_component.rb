1a451153ad3113de59bd27fa6ef51a4e159b2cc0
Patrick Baselier <patrick.baselier@gmail.com>
2020-12-29 06:57:46 +0100

    Add options

--- app/components/bs5/progress_component.rb
+++ app/components/bs5/progress_component.rb
@@ -5,15 +5,27 @@ module Bs5
     # include ComponentsHelper
     # STYLES = %i[primary secondary success danger warning info light dark].freeze
 
-    attr_reader :value
+    attr_reader :value, :color
 
     # include ActiveModel::Validations
     # validates :color, style: true
 
-    def initialize(value)
+    def initialize(value, label: nil, color: nil)
       @value = value
+      @label = label
+      @color = color
     end
 
+    def label
+      return unless @label
+
+      case @label
+      when String
+        @label
+      else
+        "#{value}%"
+      end
+    end
     # def before_render
     #   raise errors.full_messages.to_sentence if invalid?
     # end
@@ -22,14 +34,22 @@ module Bs5
     #   content.present?
     # end
 
-    # def component_class
-    #   class_names = Array(@options[:class])
-    #   class_names << 'progress'
-    #   class_names.join(' ')
-    # end
+    def bar_class
+      class_names = %w[progress-bar]
+      class_names << contextual_class
+      class_names.join(' ')
+    end
 
-    # def contextual_class
-    #   "alert-#{@color}"
-    # end
+    def contextual_class
+      return unless color?
+
+      "bg-#{color}"
+    end
+
+    %i[color].each do |name|
+      define_method("#{name}?") do
+        !instance_variable_get("@#{name}").nil?
+      end
+    end
   end
 end
--- spec/helpers/bs5/bs5/bs5_progress_spec.rb
+++ spec/helpers/bs5/bs5/bs5_progress_spec.rb
@@ -5,15 +5,32 @@ require 'rails_helper'
 module Bs5
   RSpec.describe ComponentsHelper, type: :helper do
     describe '#bs5_progress' do
-      subject { helper.bs5_progress(value) }
+      subject { helper.bs5_progress(value, options) }
 
       let(:value) { 25 }
       let(:options) { {} }
 
       describe 'markup' do
-        it {
-          is_expected.to have_selector('.progress .progress-bar[role="progressbar"][style="width: 25%"][aria-valuenow="25"][aria-valuemin="0"][aria-valuemax="100"]')
-        }
+        it { is_expected.to have_selector('.progress .progress-bar[role="progressbar"][style="width: 25%"][aria-valuenow="25"][aria-valuemin="0"][aria-valuemax="100"]') }
+      end
+
+      describe 'with `label`' do
+        describe 'with boolean' do
+          let(:options) { { label: true } }
+
+          it { is_expected.to have_selector('.progress .progress-bar', text: '25%') }
+        end
+
+        describe 'with string'
+        let(:options) { { label: 'Lorem Ipsum' } }
+
+        it { is_expected.to have_selector('.progress .progress-bar', text: 'Lorem Ipsum') }
+      end
+
+      describe 'with `color`' do
+        let(:options) { { color: :secondary } }
+
+        it { is_expected.to have_selector('.progress .progress-bar.bg-secondary') }
       end
     end
   end
